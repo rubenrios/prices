69f05efe3abe668fedc5cdd43b0445a508ddc70c
rubenrios <ruben.rios@udc.es>
2025-03-06 01:07:33 +0100

    Init project, create structure, added yml and sql

---
+++ src/test/java/com/rubenrbr/prices/PricesApplicationTests.java
@@ -0,0 +1,11 @@
+package com.rubenrbr.prices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricesApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
